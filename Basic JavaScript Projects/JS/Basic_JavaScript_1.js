b54190659f5a4033d689e97d7a3a00891d5b841a
jmschulsinger <56856035+jmschulsinger@users.noreply.github.com>
2019-10-26 18:34:23 -0700

    Add files via upload

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_1.js
@@ -0,0 +1,14 @@
+var Family = "The Schulsinger's", Dad = "Jeff", Mom = "Dina", Son = "Jordan", Daughter = "Jillian"
+    var Daughter = Daughter.fontcolor("pink");
+    var Dad = Dad.fontcolor("green");
+    var Mom = Mom.fontcolor("purple");
+    var Son = Son.fontcolor("red");
+    var Family = Family.fontcolor("blue");
+
+document.write(Family, Son, Mom, Dad, Daughter);
+
+var x = 5;
+var y = 6;
+var z = x + y;
+
+document.write(z);
